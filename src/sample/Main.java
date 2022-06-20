e8806cef3ef02df4b2d0a05c43150b1d72d80bb3
Rafael Cruz <rafaeloribata@gmail.com>
2022-06-20 17:53:06 -0300

    Adicionando algumas funções

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,25 @@
 package sample;
 
+import gui.util.Alerts;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    private static Scene mainScene;
 
     @Override
     public void start(Stage primaryStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
             Parent parent = loader.load();
-            Scene mainScene = new Scene(parent);
+            mainScene = new Scene(parent);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
@@ -24,7 +28,13 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
+
+    public static Scene getMainScene(){
+        return mainScene;
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
+
 }
